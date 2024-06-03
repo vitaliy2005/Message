33aef42ba55202f9cabf90a0e9da0750620855c6
vitaliy2005 <svitala500@gmail.com>
2024-06-03 15:01:16 +0700

    check

---
+++ build/Desktop-Debug/CMakeFiles/message.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "" "message_autogen/timestamp" "custom" "message_autogen/deps"
+  "/home/warg/Work/qt/message/main.cpp" "CMakeFiles/message.dir/main.cpp.o" "gcc" "CMakeFiles/message.dir/main.cpp.o.d"
+  "/home/warg/Work/qt/message/mainwindow.cpp" "CMakeFiles/message.dir/mainwindow.cpp.o" "gcc" "CMakeFiles/message.dir/mainwindow.cpp.o.d"
+  "/home/warg/Work/qt/message/build/Desktop-Debug/message_autogen/mocs_compilation.cpp" "CMakeFiles/message.dir/message_autogen/mocs_compilation.cpp.o" "gcc" "CMakeFiles/message.dir/message_autogen/mocs_compilation.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/Desktop-Debug/CMakeFiles/message_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/message_autogen"
+  "message_autogen/include/ui_mainwindow.h"
+  "message_autogen/mocs_compilation.cpp"
+  "message_autogen/timestamp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/message_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
