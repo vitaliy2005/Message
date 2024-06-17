840d525f66e48534bbf8b96b6d603767583b7167
vitaliy2005 <svitala500@gmail.com>
2024-06-18 06:42:28 +0700

    add build server for linux

---
+++ Client/build/Winods-Debug/CMakeFiles/client.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/client_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/client_autogen.dir/ParseCache.txt"
+  "client_autogen"
+  "CMakeFiles/client.dir/client_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/client.dir/client_autogen/mocs_compilation.cpp.o.d"
+  "CMakeFiles/client.dir/main.cpp.o"
+  "CMakeFiles/client.dir/main.cpp.o.d"
+  "CMakeFiles/client.dir/mainwindow.cpp.o"
+  "CMakeFiles/client.dir/mainwindow.cpp.o.d"
+  "client"
+  "client.pdb"
+  "client_autogen/mocs_compilation.cpp"
+  "client_autogen/timestamp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Client/build/Winods-Debug/CMakeFiles/client_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/client_autogen"
+  "client_autogen/include/ui_mainwindow.h"
+  "client_autogen/mocs_compilation.cpp"
+  "client_autogen/timestamp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/client_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
